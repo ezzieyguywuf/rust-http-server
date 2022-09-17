4dc30b7b874cb86ebbd2f1beeec56a0faf6a7e8f
Wolfgang E. Sanyer <wolfgangsanyer@google.com>
2022-09-17 00:25:38 +0000

    Add error checking for Tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listener) => listener,
+        Err(error) => {
+            println!("Error connecting: {:?}", error);
+            return;
+        }
+    };
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
